caddbc28bcd44b694831197d6d6c9c8e6b1c158c
Jennifer Nguyen <j3nniferefinn3j@gmail.com>
2017-03-02 12:44:03 -0600

    final for class pres

--- JS/2.js
+++ JS/2.js
@@ -1,30 +1,42 @@
 $(document).ready(function(){
-	var canvas = $("#myCanvas").get(0);
-
+	// var canvas = $("#myCanvas").get(0);
+var context = $('canvas').get(0).getContext("2d"); 
 	$('#pbutt').on('click', function(){
 	  // $('body').append('<img src="' + userInput + ' "height="90px">');
 	  var userInput= $('#picval').val();
 	  var img = new Image();
 		img.src = userInput;
 	// once the user presses go
-		var canvas = $("#myCanvas").get(0);
+		// var canvas = $("#myCanvas").get(0);
 	// to get the x 
-		var context = canvas.getContext("2d"); 
+		
 	// canvas set up
-		$(userInput).load(function(){
+		$(img).load(function(){
 			context.drawImage(img, 0, 0);
 			// putting img on the canvas @ (0,0)
-			data = context.getImageData(x,y,1,1).data;
-			$('#info').apppend(data);
 		});
 
 
 	});
 
 	
-	$(canvas).click(function(event){
+	$("canvas").click(function(event){
+		var actualX;
+		var actualY;
 		// make sure x,y coord shows up on the side 
+	
+		var canvasGap = $("canvas").offset();
+			actualX = Math.floor(event.pageX- canvasGap.left);
+			actualY = Math.floor(event.pageY- canvasGap.top);
 		$('#info').append("(" + event.pageX + ", " + event.pageY + ")");
+		var data = context.getImageData(actualX,actualY,1,1).data;
+		
+
+		var pixelData = context.getImageData(actualX, actualY, 1, 1);
+		var data = pixelData.data;
+		var pixelColor = "rgb(" +data[0] + ", " + data[1]+ ", " + data[2]+ ", " + data[3] + ")";
+		$('#info').apppend(pixelColor);
+		$("#display").css("backgroundColor", pixelColor);
 	});
 });
 	
