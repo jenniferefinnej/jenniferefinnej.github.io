95e810542de6867c0f7c2a56437c93f1efe69bf6
Jennifer Nguyen <j3nniferefinn3j@gmail.com>
2017-03-02 07:14:22 -0600

    css and js being edited, mainly canvas stuff

--- JS/2.js
+++ JS/2.js
@@ -1,10 +1,21 @@
+var userInput = $('#picval').val();
 $('#pbutt').on('click', function(){
-  var userInput = $('#picval').val();
   $('body').append('<img src="' + userInput + ' "height="90px">');
 });
 
 $(document).ready(function(){
 	var canvas = $("#myCanvas").get(0);
-	<!-- to get as dom element -->
-	var context = canvas.getContext("2d");
+	// to get the x 
+	var context = canvas.getContext("2d"); 
+	$(userInput).load(function(){
+		context.drawImage(image, 0, 0);
+		// putting img on the canvas @ (0,0)
+	}});
+
+	$(canvas).click(function(event){
+		// make sure x,y coord shows up on the side 
+		$('body').append("(" + event.pageX + ", " + event.pageY + ")");
 	});
+});
+	
+	// type of grid
