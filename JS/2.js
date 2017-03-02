9deb4cec0c5dec29e7a66b15dbe86d4974ab1faa
Jennifer Nguyen <j3nniferefinn3j@gmail.com>
2017-03-02 09:48:15 -0600

    linked 2 htmls

--- JS/2.js
+++ JS/2.js
@@ -1,24 +1,30 @@
-var userInput = $('#picval').val();
-$('#pbutt').on('click', function(){
-  $('body').append('<img src="' + userInput + ' "height="90px">');
-});
-
 $(document).ready(function(){
-	var img = new Image();
-	img.src = userInput;
 	var canvas = $("#myCanvas").get(0);
-	// to get the x 
-	var context = canvas.getContext("2d"); 
-	// canvas set up
-	$(userInput).load(function(){
-		context.drawImage(image, 0, 0);
-		// putting img on the canvas @ (0,0)
-	data = context.getImageData(x,y,1,1).data;
-	}});
 
+	$('#pbutt').on('click', function(){
+	  // $('body').append('<img src="' + userInput + ' "height="90px">');
+	  var userInput= $('#picval').val();
+	  var img = new Image();
+		img.src = userInput;
+	// once the user presses go
+		var canvas = $("#myCanvas").get(0);
+	// to get the x 
+		var context = canvas.getContext("2d"); 
+	// canvas set up
+		$(userInput).load(function(){
+			context.drawImage(img, 0, 0);
+			// putting img on the canvas @ (0,0)
+			data = context.getImageData(x,y,1,1).data;
+			$('#info').apppend(data);
+		});
+
+
+	});
+
+	
 	$(canvas).click(function(event){
 		// make sure x,y coord shows up on the side 
-		$('body').append("(" + event.pageX + ", " + event.pageY + ")");
+		$('#info').append("(" + event.pageX + ", " + event.pageY + ")");
 	});
 });
 	
