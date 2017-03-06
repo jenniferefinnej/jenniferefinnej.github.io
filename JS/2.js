bc7903bc1b4ce67cc5dcf409af29371d88060248
Jennifer Nguyen <j3nniferefinn3j@gmail.com>
2017-03-06 11:45:05 -0600

    final for now

--- JS/2.js
+++ JS/2.js
@@ -4,7 +4,9 @@ var context = $('canvas').get(0).getContext("2d");
 	$('#pbutt').on('click', function(){
 	  // $('body').append('<img src="' + userInput + ' "height="90px">');
 	  var userInput= $('#picval').val();
-	  var img = new Image();
+	  	var img = new Image();
+	  	img.crossOrigin ='';
+  		// img.src = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/koala.jpg';
 		img.src = userInput;
 
 		$(img).load(function(){
@@ -24,14 +26,17 @@ var context = $('canvas').get(0).getContext("2d");
 		var canvasGap = $("canvas").offset();
 			actualX = Math.floor(event.pageX- canvasGap.left);
 			actualY = Math.floor(event.pageY- canvasGap.top);
-		$('#info').append("(" + event.pageX + ", " + event.pageY + ")");
-		var data = context.getImageData(actualX,actualY,1,1).data;
+			console.log(actualX);
+			console.log(actualY);
+		// $('#info').append("(" + event.pageX + ", " + event.pageY + ")");
+		var data = context.getImageData(actualX,actualY,1,1);
 		
 
-		var pixelData = context.getImageData(actualX, actualY, 1, 1);
-		var data = pixelData.data;
-		var pixelColor = "rgba(" +data[0] + ", " + data[1]+ ", " + data[2]+ ", " + data[3] + ")";
-		$('#info').apppend(pixelColor);
+		// var pixelData = context.getImageData(actualX, actualY, 1, 1);
+		// var data = pixelData.data;
+		console.log(data);
+		var pixelColor = "<p>rgba(" + data[0] + ", " + data[1]+ ", " + data[2]+ ", " + data[3] + ")</p>";
+		$('#info').append(pixelColor);
 		$("#display").css("backgroundColor", pixelColor);
 	});
 });
